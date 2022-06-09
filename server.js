577433548e74dcf41015f3e3f16db9c7809cd49b
hasan <hasanjonsadullayev97@gmail.com>
2022-06-09 11:36:51 -0700

    Travel Book

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const connectDB = require("./config/db");
+const connectDB = require("./server/config/db");
 require("colors");
 
 //Connect to database
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 //Api Routes
-app.use("/api/travels", require("./routes/travelRoutes"));
+app.use("/api/travels", require("./server/routes/travelRoutes"));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
